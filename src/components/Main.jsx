98a66ea935a7d1046966b78094468d118d95ac6b
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-03-17 13:06:26 +0100

    Update styles

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,7 @@ import '../styles/Main.css';
 export default function Main() {
   return (
     <main>
-      <div className="box"></div>
+      <div className="container"></div>
     </main>
   );
 }
