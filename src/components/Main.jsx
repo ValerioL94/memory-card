68f2377e5b118cfb7fe0d253dfe6898302757e64
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-03-18 11:53:31 +0100

    Update styles

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,32 +5,27 @@ export default function Main() {
   const [category, setCategory] = useState('');
   return (
     <main>
-      <div className="container">
-        {category ? (
-          ''
-        ) : (
-          <div className="playerChoice">
-            <h1>Choose a category</h1>
-            <div className="choiceWrapper">
-              <div className="choice" id="sorceries">
-                <h2>Sorceries</h2>
-                <button type="button" onClick={() => setCategory('sorceries')}>
-                  <img src="images/sorceryScroll.webp" alt="sorcery scroll" />
-                </button>
-              </div>
-              <div className="choice" id="incantations">
-                <h2>Incantations</h2>
-                <button
-                  type="button"
-                  onClick={() => setCategory('incantations')}
-                >
-                  <img src="images/prayerBook.webp" alt="prayer book " />
-                </button>
-              </div>
+      {category ? (
+        <div className="cardsContainer"></div>
+      ) : (
+        <div className="playerChoice">
+          <h1>Choose a category</h1>
+          <div className="choiceWrapper">
+            <div className="choice" id="sorceries">
+              <h2>Sorceries</h2>
+              <button type="button" onClick={() => setCategory('sorceries')}>
+                <img src="images/sorceryScroll.webp" alt="sorcery scroll" />
+              </button>
+            </div>
+            <div className="choice" id="incantations">
+              <h2>Incantations</h2>
+              <button type="button" onClick={() => setCategory('incantations')}>
+                <img src="images/prayerBook.webp" alt="prayer book " />
+              </button>
             </div>
           </div>
-        )}
-      </div>
+        </div>
+      )}
     </main>
   );
 }
