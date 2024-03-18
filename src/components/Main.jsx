4d7b0115bef49a689f623af1b6dcac282a65fe26
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-03-18 11:38:21 +0100

    Update main section

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,36 @@
 import '../styles/Main.css';
+import { useState } from 'react';
 
 export default function Main() {
+  const [category, setCategory] = useState('');
   return (
     <main>
-      <div className="container"></div>
+      <div className="container">
+        {category ? (
+          ''
+        ) : (
+          <div className="playerChoice">
+            <h1>Choose a category</h1>
+            <div className="choiceWrapper">
+              <div className="choice" id="sorceries">
+                <h2>Sorceries</h2>
+                <button type="button" onClick={() => setCategory('sorceries')}>
+                  <img src="images/sorceryScroll.webp" alt="sorcery scroll" />
+                </button>
+              </div>
+              <div className="choice" id="incantations">
+                <h2>Incantations</h2>
+                <button
+                  type="button"
+                  onClick={() => setCategory('incantations')}
+                >
+                  <img src="images/prayerBook.webp" alt="prayer book " />
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
     </main>
   );
 }
